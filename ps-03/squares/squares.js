90a87611de706808f2e2bbe9e3313009dd5bf940
kateterrado <kate@kateterrado.com>
2016-10-03 22:28:00 -0400

    ps-03

---
+++ ps-03/squares/squares.js
@@ -0,0 +1,16 @@
+var canvas = document.getElementById('canvas');
+var drawingPad = canvas.getContext('2d');
+drawingPad.fillStyle = "#E5E5E5";
+drawingPad.fillRect(30, 30, 20, 20);
+
+drawingPad.fillStyle = "#E5E5E5";
+drawingPad.fillRect(70, 70, 40, 40);
+
+drawingPad.fillStyle = "#E5E5E5";
+drawingPad.fillRect(140, 140, 60, 60);
+
+drawingPad.fillStyle = "#E5E5E5";
+drawingPad.fillRect(240, 240, 80, 80);
+
+drawingPad.fillStyle = "#E5E5E5";
+drawingPad.fillRect(360, 360, 100, 100);
